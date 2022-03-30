22719de9ae2ea8c2359fd2a24f6be3546de27c74
Krish Yadav <44264836+krishyadav007@users.noreply.github.com>
2022-03-30 16:17:41 +0530

    cmt

--- v15-ui-dry-tun/ui.py
+++ v15-ui-dry-tun/ui.py
@@ -1,18 +1,46 @@
 import streamlit as st
 from PIL import Image
-# m = st.markdown("""
-# <style>
-# div.stButton > button:first-child {
-#      background-color: rgb(204, 49, 49);
-# }
-# </style>""", unsafe_allow_html=True)
 
-image = Image.open('sunrise.jpg')
+padding = 0
+st.markdown(f""" <style>
+    .reportview-container .main .block-container{{
+        padding-top: {padding}rem;
+        padding-right: {padding}rem;
+        padding-left: {padding}rem;
+        padding-bottom: {padding}rem;
+    }} </style> """, unsafe_allow_html=True)
 
-col1,col2 = st.columns(2)
-col1.image(image, caption='Smoky vehicle detected')
+st.markdown(""" <style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style> """, unsafe_allow_html=True)
+st.markdown("""
+        <style>
+               .css-18e3th9 {
+                    padding-top: 0rem;
+                    padding-bottom: 10rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                }
+               .css-1d391kg {
+                    padding-top: 3.5rem;
+                    padding-right: 1rem;
+                    padding-bottom: 3.5rem;
+                    padding-left: 1rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
+st.markdown('<div style="background-color:#00285A;text-align:center;margin-bottom:5rem"><h1 style="color:#fff">AI POLLUTION INSPECTOR<h1></div>',unsafe_allow_html=True)
 
-# col2.markdown("<p style='text-align: center;'>License plate number : <strong>"+ "PT ST 300" + "</strong></p>", unsafe_allow_html=True)
-col2.markdown('License plate number : **' + "PT ST 300" + '**')
-col2.markdown('Should we send challan?')
-col2.button("Send")
+uploaded_file = st.file_uploader("Choose a file")
+cols = []
+for ii in range(10):
+    image = Image.open('sunrise.jpg')
+    tp_col1,tp_col2 = st.columns(2)
+    cols.append([tp_col1, tp_col2])
+    tp_col1.image(image, caption='Smoky vehicle detected')
+
+    # col2.markdown("<p style='text-align: center;'>License plate number : <strong>"+ "PT ST 300" + "</strong></p>", unsafe_allow_html=True)
+    tp_col2.markdown('License plate number : **' + "PT ST 300" + '**')
+    tp_col2.markdown('Should we send challan?')
+    tp_col2.button("Send", key="key"+str(ii))
